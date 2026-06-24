ae5dd41f796e68a39bbfe7f6780a0b02feccbf87
ProdyPanda <admin@prodypanda.com>
2026-06-24 03:09:56 +0100

    Update custom icon font and mappings to match regenerated C:\tek\ivra_refill_new_icons

--- lib/src/ui/ivra_icons.dart
+++ lib/src/ui/ivra_icons.dart
@@ -25,41 +25,41 @@ class IvraIcons {
 
   /// Full bottle with pump (bouteilles avec pompe pleine)
   static const IconData fullBottleWithPump =
-      IconData(0xe808, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+      IconData(0xe809, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 
   /// Empty bottle with pump (bouteilles avec pompe vide)
   static const IconData emptyBottleWithPump =
-      IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+      IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 
   /// Full bottle without pump (bouteilles sans pompe pleine)
   static const IconData fullBottleWithoutPump =
-      IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+      IconData(0xe808, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 
   /// Empty bottle without pump (bouteilles sans pompe vide)
   static const IconData emptyBottleWithoutPump =
-      IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+      IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 
   /// Full refill bottle (bouteilles de recharge pleines)
   static const IconData fullRefillBottle =
-      IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+      IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 
   /// Empty refill bottle (bouteilles de recharge vide)
   static const IconData emptyRefillBottle =
-      IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+      IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 
   /// Refill action (recharger bouteilles)
   static const IconData refillAction =
-      IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+      IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 
   /// Replace bottle action (remplacer la bouteille)
   static const IconData replaceAction =
-      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+      IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 
   /// Bottle volume indicator (volume de la bouteille)
   static const IconData bottleVolume =
-      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 
   /// Refill bottle volume indicator (volume de la bouteille de recharge)
   static const IconData refillBottleVolume =
-      IconData(0xe809, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
